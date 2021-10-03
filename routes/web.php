f992f2ff48f0f06973d75cf22f13179888507196
philliphemleb <github@phillip.hemleb.de>
2021-10-03 19:48:20 +0200

    An unauthenticated user will now be redirected to the start site

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TwitterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return 'Nothing to see here, Bye!';
-});
+})->name('home');
--- tests/Unit/Controllers/AuthControllerTest.php
+++ tests/Unit/Controllers/AuthControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\Unit\Controllers;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
