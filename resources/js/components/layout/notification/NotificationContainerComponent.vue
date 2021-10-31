9ae5455723ed750380eecd4d1d3538dba2db7e87
philliphemleb <github@phillip.hemleb.de>
2021-10-31 12:32:33 +0100

    [PA-17] Notifications can only be 20% of the width now

--- resources/js/components/layout/notification/NotificationContainerComponent.vue
+++ resources/js/components/layout/notification/NotificationContainerComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="absolute right-0 mt-n2 z-50" style="min-width: 20%;">
+    <div class="absolute right-0 mt-n2 z-50" style="min-width: 20%; max-width: 20%;">
         <slot></slot>
     </div>
 </template>
